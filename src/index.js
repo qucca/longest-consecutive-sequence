6c3ed44ef64166403994e68d1bc4322ac80afc45
Dmitry Khakhomov <dmi09@hotmail.com>
2018-09-27 17:04:26 +0300

    Finishing task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let count = 1;
+    let result = 1;
+    let nums = new Set(array);
+    if (array.length > 1) {
+        for (let value of nums) {
+            if (nums.has(value + 1)) {
+                let curValue = value;
+                count = 1
+                while (nums.has(curValue + 1)) {
+                    count += 1;
+                    curValue += 1;
+                }
+                if (count >= result) {
+                    result = count;
+                }
+            }
+        }
+    }
+    else return array.length;
+    return result;
 }
